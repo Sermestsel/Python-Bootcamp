015f55fb1dde44ee0252c35728d5256dcadd70c0
Umut Karahisar <karahisaru@gmail.com>
2026-07-25 16:21:36 +0300

    Veri Yapıları Arasındaki Farklar : Notlar

---
+++ 2_python_temel_yapilar/veri_yapilari_farklar.py
@@ -0,0 +1,37 @@
+# Veri Yapıları Arasındaki Farklar: Liste, Tuple, Sözlük, Set
+
+"""
+
+Liste:
+    -Sıralıdır.
+    - Değiştirilebilir.
+    - Tekrar eden elemanlara izin verir.
+    -Örnek : Liste=[a,b,c]
+    -Kullanım: Eleman sırası önemliyse, veri güncellenecekse
+    Liste, numpy Array'ın temelini oluşturur.
+
+Tuple:
+    -Sıralıdır.
+    -Değiştirilemez.
+    -Tekrar eden elemanlara izin verir.
+    - Örnek : Tuple = (1,2,3,4)
+    -Kullanım: Veri sabit kalacaksa, güvenli yapı gerekiyorsa.
+
+Sözlük (Dictionary):
+    -anahtar-değer(key-value pair)
+    -anahtarlar benzersizdir, değerler tekrar edebilir
+    - değiştirilebilir.
+    -Örnek : ogrenci {"isim":"umut", "yas":35}
+    -Anlamlı veri saklamak, etiketli veri tutmak.
+    -pandas dataframe temelini oluşturur. 
+
+Set:
+    -Sırasızdır.
+    -Tekrar eden elemanları kabul etmez.
+    -Veri Değiştirilebilir.
+    -Örnek : set(1,2,3,4)
+    -Kullanım:  Tekrar eden değerleri temizlemek için ve küme işlemleri yapmak için kullanılır.
+    
+      
+
+"""
